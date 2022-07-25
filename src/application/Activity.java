a4007416e739e0097328a1e6c3a610d773231e39
rickm <rickm@MSI>
2022-07-24 22:02:12 -0600

    Added User and Activity classes

---
+++ src/application/Activity.java
@@ -0,0 +1,43 @@
+package application;
+
+public class Activity {
+
+	private boolean low;
+	private boolean medium;
+	private boolean high;
+	private int duration;
+	
+	boolean isLow() {
+		return low;
+	}
+	void setLow(boolean low) {
+		this.low = low;
+	}
+	boolean isMedium() {
+		return medium;
+	}
+	void setMedium(boolean medium) {
+		this.medium = medium;
+	}
+	
+	boolean isHigh() {
+		return high;
+	}
+	void setHigh(boolean high) {
+		this.high = high;
+	}
+	
+	int getDuration() {
+		return duration;
+	}
+	void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	int calcCaloriesBurned(int activity, int duration, double BMi) {
+		return 0;
+	}
+
+	
+	
+}
---
+++ src/application/User.java
@@ -0,0 +1,63 @@
+package application;
+
+public class User {
+	
+	//Creating instance variables
+	private double weight;
+	private double height;
+	private int age;
+	private int activityLevel; //Average activity level
+	private int dailyCalories;
+	private double BMI;
+	
+	
+	//Getters and Setters for instance variables
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getActivity() {
+		return activityLevel;
+	}
+	public void setActivity(int activity) {
+		this.activityLevel = activity;
+	}
+	public int getDailyCalories() {
+		return dailyCalories;
+	}
+	public void setDailyCalories(int dailyCalories) {
+		this.dailyCalories = dailyCalories;
+	}
+	
+	
+	//Creating constructors
+	double getBMI(double height, double weight) {
+		//BMI calculation:
+		return 0;
+	}
+	
+	int calculateDailyCalories(double BMI, int age, int activityLevel) {
+		return 0;
+	}
+	
+	void addActivity() {
+		
+	}
+	int addMealEaten(int caloriesEaten) {
+		return dailyCalories;
+	}
+}
