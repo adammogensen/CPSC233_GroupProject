3ec1220028dd1041bd0b803b4478b7847a1c3e44
Adam Mogensen <Adams PC>
2022-08-15 15:04:55 -0600

    Added documentation to Food Regulation Class

--- src/application/FoodRegulation.java
+++ src/application/FoodRegulation.java
@@ -2,12 +2,17 @@ package application;
 
 
 public interface FoodRegulation {
+	
+	/*Constants to regulate the maximum number of food items from each food group
+	that can be added into a meal*/
 
 	static final int  MAXMEALGRAIN = 4;
 	static final int  MAXMEALFRUIT = 5;
 	static final int  MAXMEALDAIRY = 4;
 	static final int  MAXMEALPROTEIN = 2;
 	
+	/*flag that will be used in food classes (Protein, Dairy, Grain, Fruit) to 
+	check if current amount of food in a meal exceeds maximum allowance */
 	boolean canEat();
 	
 
